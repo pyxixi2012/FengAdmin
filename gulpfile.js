78847a474f35a0d40e31d30eba09675b71483c34
liuyongliang <hiericliu@gmail.com>
2016-05-27 14:11:38 +0800

    增加页面自动刷新及监控less

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,41 @@
+var gulp = require("gulp"),
+    less = require("gulp-less"),
+    browserSync = require("browser-sync"),
+    path = {
+        HTML : "./*.html",
+        LESS : "less/*.less",
+        CSS : "css",
+        JS : "js/*.js"
+    };
 
-var gulp   = require('gulp'),
-    livereload = require('gulp-livereload'),
-  	concat = require('gulp-concat'), //合并文件
-  	jshint = require('gulp-jshint'), //检测js
-  	uglify = require('gulp-uglify'),//js压缩
-  	rename = require('gulp-rename'), //重命名
-  	minifycss = require('gulp-minify-css'); //css压缩
- 
-    gulp.task('default', function(){
-        gulp.start('styles','scripts');
+gulp.task("serve", ["less", "js-watch", "html"], function() {
+    browserSync.init({
+        server : "./"
     });
+
+    gulp.watch(path.LESS, ["less"]);
+    gulp.watch(path.JS, ["js-watch"]);
+    gulp.watch(path.HTML, ["html"]);
+    gulp.watch(path.HTML).on("change", function() {
+        browserSync.reload;
+    });
+});
+
+gulp.task("less", function() {
+    gulp.src(path.LESS)
+        .pipe(less())
+        .pipe(gulp.dest(path.CSS))
+        .pipe(browserSync.stream());
+})
+
+gulp.task("js-watch", function() {
+    gulp.src(path.JS)
+    .pipe(browserSync.stream());
+})
+
+gulp.task("html", function() {
+    gulp.src(path.HTML)
+    .pipe(browserSync.stream());
+})
+
+gulp.task("default", ["serve"])
