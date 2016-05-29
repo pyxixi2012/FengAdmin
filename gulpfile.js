d08d5ea26d89da2c4cc324a3a91bc2ece0a207f8
liuyongliang <hiericliu@gmail.com>
2016-05-29 12:55:00 +0800

    更新gulp配置文件，增加bootstrap less

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,21 @@ var gulp = require("gulp"),
     sourcemaps = require('gulp-sourcemaps'),
     path = {
         HTML : "./*.html",
+        LESSFILE : "less/*.less",
         LESS : "less/feng.less",
+        LESSBOOTSTRAPFILE : "less/bootstrap/**/*.less",
+        LESSBOOTSTRAP : "less/bootstrap.less",
         CSS : "css",
         JS : "js/*.js"
     };
 
-gulp.task("serve", ["less", "js-watch", "html"], function() {
+gulp.task("serve", ["less", "lessbootstrap", "js-watch", "html"], function() {
     browserSync.init({
         server : "./"
     });
 
-    gulp.watch(path.LESS, ["less"]);
+    gulp.watch(path.LESSFILE, ["less"]);
+    gulp.watch(path.LESSBOOTSTRAPFILE, ["lessbootstrap"]);
     gulp.watch(path.JS, ["js-watch"]);
     gulp.watch(path.HTML, ["html"]);
     gulp.watch(path.HTML).on("change", function() {
@@ -27,16 +31,23 @@ gulp.task("less", function() {
         .pipe(less())
         .pipe(gulp.dest(path.CSS))
         .pipe(browserSync.stream());
+});
+
+gulp.task("lessbootstrap", function() {
+    gulp.src(path.LESSBOOTSTRAP)
+        .pipe(less())
+        .pipe(gulp.dest(path.CSS))
+        .pipe(browserSync.stream());
 })
 
 gulp.task("js-watch", function() {
     gulp.src(path.JS)
     .pipe(browserSync.stream());
-})
+});
 
 gulp.task("html", function() {
     gulp.src(path.HTML)
     .pipe(browserSync.stream());
-})
+});
 
-gulp.task("default", ["serve"])
+gulp.task("default", ["serve"]);
