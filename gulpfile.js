28adca0d8029ce6b1fb9f0814eee7592850995b2
liuyongliang <hiericliu@gmail.com>
2016-06-01 16:02:28 +0800

    更新gulpfile.js及增加include文件

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,30 @@ var gulp = require("gulp"),
     less = require("gulp-less"),
     browserSync = require("browser-sync"),
     sourcemaps = require('gulp-sourcemaps'),
+    fileinclude  = require('gulp-file-include'),
     path = {
-        HTML : "./*.html",
+        HTML : "*.html",
+        INCLUDE : "include/*.html",
         LESSFILE : "less/*.less",
         LESS : "less/feng.less",
         LESSBOOTSTRAPFILE : "less/bootstrap/**/*.less",
-        LESSBOOTSTRAP : "less/bootstrap.less",
-        CSS : "css",
-        JS : "js/*.js"
+        LESSBOOTSTRAP : "less/bootstrap/bootstrap.less",
+        IMAGES : "images/*",
+        JS : "js/**/*.js"
     };
 
-gulp.task("serve", ["less", "lessbootstrap", "js-watch", "html"], function() {
+gulp.task("serve", ["less", "lessbootstrap", "js-watch", "images","include", "html"], function() {
     browserSync.init({
-        server : "./"
+        server : "./dist/"
     });
 
     gulp.watch(path.LESSFILE, ["less"]);
     gulp.watch(path.LESSBOOTSTRAPFILE, ["lessbootstrap"]);
     gulp.watch(path.JS, ["js-watch"]);
+    gulp.watch(path.IMAGES, ["images"]);
     gulp.watch(path.HTML, ["html"]);
+    gulp.watch(path.INCLUDE, ["html"]);
+    gulp.watch(path.INCLUDE, ["include"]);
     gulp.watch(path.HTML).on("change", function() {
         browserSync.reload;
     });
@@ -29,24 +34,41 @@ gulp.task("serve", ["less", "lessbootstrap", "js-watch", "html"], function() {
 gulp.task("less", function() {
     gulp.src(path.LESS)
         .pipe(less())
-        .pipe(gulp.dest(path.CSS))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task("lessbootstrap", function() {
     gulp.src(path.LESSBOOTSTRAP)
         .pipe(less())
-        .pipe(gulp.dest(path.CSS))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 })
 
 gulp.task("js-watch", function() {
     gulp.src(path.JS)
-    .pipe(browserSync.stream());
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("images", function() {
+    gulp.src(path.IMAGES)
+        .pipe(gulp.dest('dist/images'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task("html", function() {
     gulp.src(path.HTML)
+    .pipe(browserSync.stream());
+});
+
+gulp.task('include', function() {
+    gulp.src(path.HTML)
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: 'include/'
+        }))
+    .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream());
 });
 
